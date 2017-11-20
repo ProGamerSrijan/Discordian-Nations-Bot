80c9310b380483e9ef0538032927bc52448e3aba
ProGamerSrijan <33601605+ProGamerSrijan@users.noreply.github.com>
2017-11-20 21:20:28 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,22 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
+    if (message.content === ':ping') {
+    	message.reply('Pong!');
+        client.on('message', message => {
+    if (message.content === 'LOL') {
+    	message.reply('Witty banter innit m8!');
+         client.on('message', message => {
+    if (message.content === 'memes') {
+    	message.reply('MEMEMEMEMEMEMEMEMEMES!');
+         client.on('message', message => {
+    if (message.content === 'EXPELLIARMUS!') {
+    	message.reply('Hahaha! YOU ARE A SQUIB!'); client.on('message', message => {
+    if (message.content === ':info') {
+    	message.reply('message.channel.send({embed: {
+  color: 3447003,
+  description: "Oops! You caught me changing!"
+}});');
   	}
 });
 
